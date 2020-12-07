06650cf92557eb637d5dde6f1a57368a97049082
Nicolás Marulanda P <nicomaru@outlook.com>
2020-12-08 00:41:39 +0100

    Macros ResponseFactory - notFound, recibe un mensaje de error, por defecto retorna "No se encontraron registros." - badRequest, recibe un mensaje de error.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,13 +2,15 @@
 
 namespace App\Providers;
 
+use Illuminate\Contracts\Routing\ResponseFactory;
+use Illuminate\Http\Response;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+
     /**
      * Register any application services.
-     *
      * @return void
      */
     public function register()
@@ -18,11 +20,26 @@ class AppServiceProvider extends ServiceProvider
 
     /**
      * Bootstrap any application services.
-     *
      * @return void
      */
-    public function boot()
+    public function boot(ResponseFactory $responseFactory)
     {
-        //
+        $responseFactory->macro('notFound',
+            function ($message = 'No se encontraron registros.') use ($responseFactory) {
+                return $responseFactory->json([
+                    'error' => [
+                        'message' => $message,
+                    ],
+                ], Response::HTTP_NOT_FOUND);
+            });
+
+        $responseFactory->macro('badRequest', function ($message) use ($responseFactory) {
+            return $responseFactory->json([
+                'error' => [
+                    'message' => $message,
+                ],
+            ], Response::HTTP_BAD_REQUEST);
+        });
+
     }
 }
