fa0dad3e5757248077aef79a3bf4399787e0a5aa
Nicolás Marulanda P <nicomaru@outlook.com>
2020-12-13 13:52:47 +0100

    Tipo de unidades - Modelo - Se realiza un casting a los atributos de fecha a timestamp. - Se crea un método que comprueba si existe el registro en un lista de compra.

--- app/Models/UnitType.php
+++ app/Models/UnitType.php
@@ -2,14 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Pagination\Paginator;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
 /**
  * @property int $id
  * @property string $name
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @method UnitType find(int $id)
+ * @method Paginator paginate()
  */
 class UnitType extends Model
 {
@@ -19,4 +23,18 @@ class UnitType extends Model
     protected $table = 'unit_types';
 
     protected $fillable = ['name'];
+
+    protected $casts = [
+        'created_at' => 'timestamp',
+        'updated_at' => 'timestamp',
+    ];
+
+    public function hasShoppingList(): bool
+    {
+        $count = ProductHasShoppingList::query()
+                                       ->where('unit_type_id', '=', $this->id)
+                                       ->count();
+
+        return $count > 0;
+    }
 }
