0a8e235c590f0ba63f2ac010f115fcc208a99be4
Nicolás Marulanda P <nicomaru@outlook.com>
2021-09-10 21:40:31 +0200

    Formateo de código

--- app/Http/Controllers/API/ProductShoppingListApiController.php
+++ app/Http/Controllers/API/ProductShoppingListApiController.php
@@ -14,97 +14,97 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class ProductShoppingListApiController extends Controller
 {
-
+    
     private ValidationFactory $validationFactory;
-
+    
     private array $rules = [
-        'units_per_product' => 'sometimes|required|numeric|between:1,99',
-        'product_id'        => 'required|numeric',
-        'shopping_list_id'  => 'required|numeric',
-        'unit_type_id'      => 'required|numeric',
+            'units_per_product' => 'sometimes|required|numeric|between:1,99',
+            'product_id'        => 'required|numeric',
+            'shopping_list_id'  => 'required|numeric',
+            'unit_type_id'      => 'required|numeric',
     ];
-
+    
     private ProductHasShoppingList $model;
-
+    
     private ResponseFactory $responseFactory;
-
+    
     public function __construct(
-        ResponseFactory $responseFactory,
-        ProductHasShoppingList $model,
-        ValidationFactory $validationFactory
+            ResponseFactory $responseFactory,
+            ProductHasShoppingList $model,
+            ValidationFactory $validationFactory
     ) {
         $this->responseFactory = $responseFactory;
         $this->model = $model;
         $this->validationFactory = $validationFactory;
     }
-
+    
     public function store(Request $request)
     {
         $this->validateStoreRequest($request);
         $model = $this->model->fill($request->all());
         $this->checkModelDoesntExists($model);
-
+        
         if ($model->productUnitTypeShoppingListExists()) {
             throw new BadRequestHttpException('El producto ya existe en la lista.');
         }
-
+        
         $model->setTotalCaloriesAndPrice();
-
+        
         if (!$model->save()) {
             throw new BadRequestHttpException('No se logro crear el objeto correctamente.');
         }
-
+        
         return $this->responseFactory->noContent();
     }
-
+    
     public function update(Request $request)
     {
         $this->validateStoreRequest($request);
         $model = $this->model->fill($request->all());
         $this->checkModelDoesntExists($model);
-
+        
         $currentDataModel = $model->find();
-
+        
         if (!$currentDataModel) {
             throw new NotFoundHttpException('No se encontraron registros.');
         }
-
+        
         if (!empty($model->units_per_product)) {
             $currentDataModel->units_per_product = $model->units_per_product;
         }
-
+        
         if (!is_null($model->selected)) {
             $currentDataModel->selected = $model->selected;
         }
-
+        
         $currentDataModel->setTotalCaloriesAndPrice();
-
+        
         if (!$currentDataModel->save()) {
             throw new BadRequestHttpException('No se logro actualizar el objeto correctamente.');
         }
-
+        
         $this->setTotalCaloriesAndPrice($currentDataModel->shoppingList);
-
+        
         return $this->responseFactory->noContent();
     }
-
+    
     public function destroy(Request $request)
     {
         $this->validateStoreRequest($request);
-
+        
         $model = $this->model->fill($request->all())
                              ->find();
-
+        
         if (!$model) {
             throw new NotFoundHttpException('No se encontraron registros.');
         }
-
+        
         if (!$model->delete()) {
             throw new BadRequestHttpException('No se logro eliminar el objeto correctamente.');
         }
-
+        
         $this->setTotalCaloriesAndPrice($model->shoppingList);
-
+        
         return $this->responseFactory->noContent();
     }
     
@@ -114,36 +114,36 @@ class ProductShoppingListApiController extends Controller
     private function setTotalCaloriesAndPrice(ShoppingList $shoppingList): void
     {
         $shoppingList->setTotalCaloriesAndPrice();
-
+        
         if (!$shoppingList->save()) {
             throw new HttpException(500,
-                'No se logro actualizar el total de calorías y el precio total de todas las unidades en la lista de la compra.');
+                    'No se logro actualizar el total de calorías y el precio total de todas las unidades en la lista de la compra.');
         }
     }
-
+    
     private function validateStoreRequest(Request $request): void
     {
         $validate = $this->validationFactory->make($request->all(), $this->rules);
-
+        
         foreach ($validate->errors()
                           ->all() as $message) {
             throw new BadRequestHttpException($message);
         }
     }
-
+    
     private function checkModelDoesntExists(ProductHasShoppingList $model): void
     {
         if ($model->productDoesntExists()) {
             throw new BadRequestHttpException('El producto no existe.');
         }
-
+        
         if ($model->unitTypeDoesntExists()) {
             throw new BadRequestHttpException('El tipo de unidad no existe.');
         }
-
+        
         if ($model->shoppingListDoesntExists()) {
             throw new BadRequestHttpException('El producto ya existe en la lista.');
         }
     }
-
+    
 }
